4f4633f143a0378d35ddfe966c44bc452301e94f
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-02-26 12:58:28 +0200

    Add Register page with registration-form

--- bookstore-client/src/app/app.module.ts
+++ bookstore-client/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ bookstore-client/src/app/register/register.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      'username': new FormControl('', [Validators.required]),
+      'password': new FormControl('', [Validators.required]),
+      'passwordConfirm': new FormControl('', [Validators.required]),
+      'email': new FormControl('', [Validators.required]),
+      'birthday': new FormControl('', [Validators.required]),
+      'gender': new FormControl('', [Validators.required])
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit(form) {
+    console.log('Submit is triggered');
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get passwordConfirm() {
+    return this.registerForm.get('passwordConfirm');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get birthday() {
+    return this.registerForm.get('birthday');
+  }
+
+  get gender() {
+    return this.registerForm.get('gender');
+  }
+}
