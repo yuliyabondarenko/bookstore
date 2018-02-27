13b312a85af97eae89ab607c331ef1a467791399
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-02-27 15:58:04 +0200

    Basic SecurityConfig: anyRequest.authenticated, inMemory USER & ADMIN

---
+++ bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.nixsolutions.bondarenko.bookstore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception
+  {
+    http
+        .authorizeRequests()
+        .anyRequest().authenticated()
+        .and().httpBasic()
+        .and().csrf().disable();
+  }
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+  {
+    auth
+        .inMemoryAuthentication()
+        .withUser("user").password("password").roles("USER")
+        .and().withUser("admin").password("password").roles("ADMIN");
+  }
+}
