02c77b037719b59ce453a22f05326cf6e2603e7a
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-03-09 18:14:51 +0200

    Add Book Entity & Repository

---
+++ bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.nixsolutions.bondarenko.bookstore.repository;
+
+import com.nixsolutions.bondarenko.bookstore.entity.Book;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "books", path = "books")
+public interface BookRepository extends PagingAndSortingRepository<Book, Long>
+{
+}
