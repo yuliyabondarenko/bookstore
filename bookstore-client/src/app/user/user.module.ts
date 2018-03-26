ba9d42af016e80c7477e4d2e83b9ed0511b10e8b
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-03-26 21:51:42 +0300

    Add User routing

--- bookstore-client/src/app/user/user.module.ts
+++ bookstore-client/src/app/user/user.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from "../profile/profile.component";
+import { MaterialModule } from '../material.module';
+
+
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: 'profile',
+        component: ProfileComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [UserComponent]
 })
