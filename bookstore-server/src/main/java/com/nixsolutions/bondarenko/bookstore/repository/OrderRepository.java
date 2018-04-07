1ffe00699eb09a4f55d023c97c9a74efd54665ba
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-04-07 08:01:30 +0300

    Fix api for paginate Orders by user

--- bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/repository/OrderRepository.java
+++ bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/repository/OrderRepository.java
@@ -1,16 +1,14 @@
 package com.nixsolutions.bondarenko.bookstore.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.nixsolutions.bondarenko.bookstore.entity.Order;
 
-import java.util.List;
-
 @RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
-public interface OrderRepository extends PagingAndSortingRepository<Order, Long>
-{
-  List<Order> findByUserId(@Param("userId") long userId);
-
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long> {
+  Page<Order> findByUserId(@Param("userId") long userId, Pageable pageable);
 }
