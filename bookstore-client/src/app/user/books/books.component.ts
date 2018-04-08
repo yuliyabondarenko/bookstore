3af98b44042a21307f4b53fe81434f644b22dbdc
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-04-08 12:15:24 +0300

    Display book cards on User's 'Books' page

--- bookstore-client/src/app/user/books/books.component.ts
+++ bookstore-client/src/app/user/books/books.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../../entity/book';
+import { BookService } from '../../service/book.service';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+  books: Book[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private bookService: BookService) {
   }
 
+  ngOnInit() {
+    this.getBooks();
+  }
+
+  getBooks(): void {
+
+    this.bookService.getBooks()
+      .then(response => {
+        this.books = response._embedded.books as Book[];
+
+      })
+      .catch(error => {
+          alert("Can't get books: " + error.message);
+        }
+      );
+  }
 }
