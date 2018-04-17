10c18a4beab182be6de4b1e65ceeff6da419b12a
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-04-18 00:42:17 +0300

    Revert of H2 tmp settings

--- bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/security/SecurityUserDetailsService.java
+++ bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/security/SecurityUserDetailsService.java
@@ -1,7 +1,5 @@
 package com.nixsolutions.bondarenko.bookstore.security;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
