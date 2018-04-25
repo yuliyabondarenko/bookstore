d8d88d955a1036afd78873f5cbfcc45e150a81ed
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2018-04-25 21:06:38 +0300

    Make Role repository not exported

--- bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/repository/UserRoleRepository.java
+++ bookstore-server/src/main/java/com/nixsolutions/bondarenko/bookstore/repository/UserRoleRepository.java
@@ -2,12 +2,14 @@ package com.nixsolutions.bondarenko.bookstore.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.nixsolutions.bondarenko.bookstore.entity.UserRole;
 import com.nixsolutions.bondarenko.bookstore.entity.enums.Role;
 
 @Repository
+@RestResource(exported = false)
 public interface UserRoleRepository extends CrudRepository<UserRole, Long> {
   UserRole findOneByName(@Param("name") Role role);
 }
