b7c202db246c85adfc729e731f622362f32812ac
ybondarenko <yuliya.bondarenko@nixsolutions.com>
2022-03-14 10:47:29 +0200

    Fix book image urls

--- bookstore-server/src/main/resources/data.sql
+++ bookstore-server/src/main/resources/data.sql
@@ -27,19 +27,17 @@ INSERT INTO user_account_roles (user_account_id, roles_id)
 VALUES (4, 2), (4, 3);
 
 INSERT INTO book (id, name, price, photo)
-VALUES (1, 'Юлий Алкин "Цена познания"', 12.3, 'http://fb2-epub.ru/Fantstic/Alkin/Tsiena_poznaniia-Iurii_Alkin.jpg');
+VALUES (1, 'Юлий Алкин "Цена познания"', 12.3, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1451050750l/8032162.jpg');
 INSERT INTO book (id, name, price, absent, photo)
 VALUES (2, 'Оноре де Бальзак "Тридцатилетняя женщина"', 10.2, true, 'http://irecommend.ru/sites/default/files/product-images/75276/ZtWWyVa21Kvl4bYJFfvg.jpg');
 INSERT INTO book (id, name, price, absent, photo)
-VALUES (3, 'Трейси Брайан "Оставьте брезгливость, съешьте лягушку!"',  17.4, true,'https://audioknigi.club/uploads/topics/preview/00/00/65/47/9f924e7dec.jpg');
+VALUES (3, 'Трейси Брайан "Оставьте брезгливость, съешьте лягушку!"',  17.4, true,'https://m.media-amazon.com/images/I/519QZN+rWwL.jpg');
 INSERT INTO book (id, name, price, photo)
 VALUES (4,  'Рей Бредбери - "451 градус по Фаренгейту"', 53.34, 'http://loveread.ec/img/photo_books/2039.jpg');
 INSERT INTO book (id, name, price, photo)
-VALUES (5, 'Оскар Уайльд - "Портрет Дориана Грея"', 54.4, 'https://audioknigi.club/uploads/topics/preview/00/01/50/05/601970b1ad.jpg');
+VALUES (5, 'Оскар Уайльд - "Портрет Дориана Грея"', 54.4, 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1451050750l/8032162.jpg');
 INSERT INTO book (id, name, price, photo)
 VALUES (6, 'Антуан Франсуа Прево "История кавалера де Грие и Манон Леско"', 11.44, 'https://i.livelib.ru/boocover/1000025434/200/ea35/AntuanFransua_Prevo__Istoriya_kavalera_de_Grie_i_Manon_Lesko.jpg');
-INSERT INTO book (id, name, price, photo)
-VALUES (7, 'Владимир Набоков "Лолита"', 41.44, 'https://i.livelib.ru/boocover/1000428291/200/5fe4/Vladimir_Nabokov__Lolita.jpg');
 INSERT INTO book (id, name, price, photo)
 VALUES (8, 'Виктор Гюго "Собор Парижской Богоматери"', 11.3, 'https://bookz.ru/pics/sobor-pa_507.jpg');
 INSERT INTO book (id, name, price, photo)
